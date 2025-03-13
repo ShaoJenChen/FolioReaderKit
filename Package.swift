b5bff810885f0cb76f34f05ad44861a7cca22147
ShaoJenChen <cash3310@outlook.com>
2025-03-13 11:47:30 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/ZipArchive/ZipArchive.git", from: "2.1.0"),
+        .package(url: "https://github.com/ZipArchive/ZipArchive.git", from: "2.5.0"),
         .package(url: "https://github.com/cxa/MenuItemKit.git", from: "3.0.0"),
         .package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", from: "0.6.0"),
         .package(url: "https://github.com/tadija/AEXML.git", from: "4.2.0"),
